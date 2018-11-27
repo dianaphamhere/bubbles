4df562501f830b8350cb19ee92093f1e38cc8793
Diana Pham <dianaphamis@gmail.com>
2018-11-26 20:39:25 -0800

    - Created bubble area box - Created container to help center everything on the page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var w = document.getElementById("wrapper");
+var w = document.getElementById("bubbleArea");
+var btn = document.getElementById("btn");
 
 w.addEventListener("click", function(){
 
@@ -6,4 +7,11 @@ w.addEventListener("click", function(){
 	newB.setAttribute("class", "bubble");
 	newB.setAttribute("id", "bubble1");
 	w.appendChild(newB);
+})
+
+btn.addEventListener("click", function(){
+	var bubbles = document.getElementsByClassName("bubble");
+	while(bubbles.length > 0){
+		bubbles[0].parentNode.removeChild(bubbles[0]);
+	}
 })
